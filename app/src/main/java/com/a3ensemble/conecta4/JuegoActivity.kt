e688ed603e83b957bd8d17a8895aa8d6231e88e3
joan-tg <joantormogarcia@gmail.com>
2020-05-19 20:15:43 +0200

    Creación Actividad del Juego (inicio) y recurso layout

---
+++ app/src/main/java/com/a3ensemble/conecta4/JuegoActivity.kt
@@ -0,0 +1,12 @@
+package com.a3ensemble.conecta4
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class JuegoActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
